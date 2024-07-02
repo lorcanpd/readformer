d0597c11c09126236f03a8faae96920261f9d078
Lorcan <lorcanpd@gmail.com>
2024-07-02 12:38:29 +0100

    increaase single GPU memory to max

--- estimate_memory_reqs.sh
+++ estimate_memory_reqs.sh
@@ -3,10 +3,10 @@
 #BSUB -q gpu-normal
 #BSUB -o logs/memory_requirements_%J.out
 #BSUB -e logs/memory_requirements_%J.err
-#BSUB -M 51200
+#BSUB -M 81920
 #BSUB -n 4
-#BSUB -gpu "mode=shared:num=1:gmem=40960::gmodel=NVIDIAA100_SXM4_80GB"
-#BSUB -R "select[mem>51200] rusage[mem=51200] span[hosts=1]"
+#BSUB -gpu "mode=shared:num=1:gmem=81920::gmodel=NVIDIAA100_SXM4_80GB"
+#BSUB -R "select[mem>81920] rusage[mem=81920] span[hosts=1]"
 #BSUB -W 00:15
 
 READFORMER_DIR="/lustre/scratch126/casm/team274sb/lp23/readformer/readformer"
