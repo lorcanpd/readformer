b458a4e51363ec6bbdd356edc7c26951835e041a
Lorcan <lorcanpd@gmail.com>
2024-08-01 11:53:40 +0100

    exclusive process

--- submit_pretraining.sh
+++ submit_pretraining.sh
@@ -48,7 +48,7 @@ for scale in "${SCALES[@]}"; do
 #BSUB -e ${LOG_DIR}/${NAME}_%J.err
 #BSUB -M ${MEMORY}
 #BSUB -n ${CORES}
-#BSUB -gpu "mode=shared:num=1:gmem=${GPU_MEMORY}"
+#BSUB -gpu "num=1:mode=exclusive_process:j_exclusive=yes"
 #BSUB -R "select[mem>${MEMORY}] rusage[mem=${MEMORY}] span[hosts=1]"
 #BSUB -W 1:00
 
